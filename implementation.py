6d18e6a9fcb3a25071756a42b959b3d50426f663
Sajti Martin <sajti.martin@gmail.com>
2024-09-24 19:17:06 +0200

    Search switching components done

--- implementation.py
+++ implementation.py
@@ -6,6 +6,7 @@ import algotom.io.loadersaver as losa
 from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 import math
+import cv2
 
 
 def make_angle_list(number_of_projections):
@@ -31,16 +32,66 @@ def print_progress(progress):
         print("Progress: " + str(progress))
 
 
-def test_reconstruction():
-    original_image = load_image("./sample_pictures/batman_bin_hires.png")
+# Check if the corners of the search box is a switching component
+# The box is defined as the following:
+# a ---- b
+# |      |
+# c ---- d
+def check_if_switching_component(a, b, c, d):
+    return a == d and c == b and a != b
+
+
+def get_search_box_values(image, values_list, x_offset, y_offset):
+    image_size = image.shape[0]
+
+    for y in range(image_size - y_offset):
+        for x in range(image_size - x_offset):
+            is_switching_component = check_if_switching_component(image[y][x], image[y][x + x_offset],
+                                                                  image[y + y_offset][x],
+                                                                  image[y + y_offset][x + x_offset])
+            if is_switching_component:
+                values_list.append(((x, y), (x + x_offset, y + y_offset)))
+
+
+def get_list_of_switching_components(image):
+    x_search_offset = 1
+    y_search_offset = 1
+
+    image_size = image.shape[0]
+
+    # Contains the switching component coordinates in (x, y) form in 4 element tuples as follows:
+    # a ---- b
+    # |      |
+    # c ---- d
+    # (a, b, c, d)
+    values_list = []
+
+    while x_search_offset != image_size and y_search_offset != image_size:
+        get_search_box_values(image, values_list, x_search_offset, y_search_offset)
+        x_search_offset += 1
+        if x_search_offset != image_size:
+            get_search_box_values(image, values_list, x_search_offset, y_search_offset)
+
+        x_search_offset -= 1
+        y_search_offset += 1
+
+        if x_search_offset != image_size:
+            get_search_box_values(image, values_list, x_search_offset, y_search_offset)
+
+        x_search_offset += 1
+
+    return values_list
+
+
+def test_reconstruction(image):
     test_projections = [180, 160, 140, 120, 100, 80, 60, 40, 30, 20, 10, 5]
     MSE_list = []
     progress = 1
 
     for projection_count in test_projections:
-        reconstructed = make_reconstructed_image(projection_count, original_image)
+        reconstructed = make_reconstructed_image(projection_count, image)
         save_image("./reconstructed/recon_image_" + str(projection_count) + ".png", reconstructed)
-        MSE_list.append(get_MSE_from_images(original_image, reconstructed))
+        MSE_list.append(get_MSE_from_images(image, reconstructed))
 
         print_progress(progress)
         progress += 1
@@ -49,4 +100,7 @@ def test_reconstruction():
     plt.show()
 
 
-test_reconstruction()
+original_image = cv2.imread("./sample_pictures/switching_1.png", flags=cv2.IMREAD_GRAYSCALE)
+# test_reconstruction(original_image)
+components = get_list_of_switching_components(original_image)
+print(components)
